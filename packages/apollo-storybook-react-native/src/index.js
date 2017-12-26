97386cdde80239ffba62d47c2fa256008e2bdb44
Abhi Aiyer <abhiaiyer91@gmail.com>
2017-12-25 21:28:02 -0800

    chore(updates): update to modules

--- packages/apollo-storybook-react-native/src/index.js
+++ packages/apollo-storybook-react-native/src/index.js
@@ -1,54 +1,7 @@
 import React from 'react';
 import { View } from 'react-native';
-import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
-import {
-  makeExecutableSchema,
-  addMockFunctionsToSchema,
-  addResolveFunctionsToSchema,
-} from 'graphql-tools';
-import { graphql, print } from 'graphql';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { ApolloLink, Observable } from 'apollo-link';
-
-/**
- * Delay for mock latency
- */
-function delay(ms) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve();
-    }, ms);
-  });
-}
-
-/**
- * Create an Apollo Link
- */
-function createLink(schema, rootValue = {}, context = {}, options = {}) {
-  const delayMs = (options && options.delayMs) || 300;
-  return new ApolloLink((operation) => {
-    return new Observable((observer) => {
-      const { query, operationName, variables } = operation;
-      delay(delayMs)
-        .then(() => {
-          return graphql(
-            schema,
-            print(query),
-            rootValue,
-            context,
-            variables,
-            operationName
-          );
-        })
-        .then((result) => {
-          observer.next(result);
-          observer.complete();
-        })
-        .catch(observer.error.bind(observer));
-    });
-  });
-}
+import createClient from 'apollo-storybook-core';
 
 export default function initializeApollo({
   typeDefs,
@@ -62,25 +15,16 @@ export default function initializeApollo({
   // fragmentMatcher such as IntrospectionFragmentMatcher, etc.
   cacheOptions = {},
 }) {
-  const schema = makeExecutableSchema({ typeDefs });
 
-  if (!!mocks) {
-    addMockFunctionsToSchema({
-      schema,
-      mocks,
-    });
-  }
-
-  if (!!typeResolvers) {
-    addResolveFunctionsToSchema(schema, typeResolvers);
-  }
-
-  const graphqlClient = new ApolloClient({
-    addTypename: true,
-    cache: new InMemoryCache(cacheOptions),
-    link: createLink(schema, rootValue, context, apolloLinkOptions),
-    connectToDevTools: true,
-    ...apolloClientOptions,
+  const graphqlClient = createClient({
+    mocks,
+    apolloLinkOptions,
+    apolloClientOptions,
+    typeResolvers,
+    typeDefs,
+    rootValue,
+    context,
+    cacheOptions,
   });
 
   function StorybookProvider({ children }) {
